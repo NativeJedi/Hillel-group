577aecd36e34270c2a0bf32ea581a3d6c147f3ac
Artem Osmak <artem.osmak@ukr.net>
2020-08-23 23:09:45 +0300

    second mission

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 'use strict';
 
 
-const user = { firstName: 'Ivan', lastName: 'Vasylchenko'};
+//mission 1
 
-function addFullName () {
-   return `${user.firstName} ${user.lastName} ${user.fullName = user.firstName + ' ' + user.lastName}`;
+// const user = { firstName: 'Ivan', lastName: 'Vasylchenko'};
+
+// function addFullName () {
+//    return `${user.firstName} ${user.lastName} ${user.fullName = user.firstName + ' ' + user.lastName}`;
+// };
+
+// addFullName(user);
+
+// console.log(user);
+
+
+
+// mission 2
+
+const users = { firstName: undefined, lastName: 'Holy', fullName: 'Jack Holy'};
+
+function restoreNames(users) {
+  const name = users.fullName.split(' ');
+  users.firstName = name[0];
 };
 
-addFullName(user);
+restoreNames(users);
 
-console.log(user);
+console.log(users);
