3c6101e1c83994f18135a110dafc03325e2dee42
Slava <19uvs99@gmail.com>
2020-08-25 11:17:41 +0300

    1

--- src/index.js
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line import/named
-import { sum } from './math';
-import './style.css';
-import './style.scss';
-
-console.log(sum(2, 3));
-
-console.log(document.body.children);
--- src/math.js
+++
@@ -1,1 +0,0 @@
-export default function sum(a, b) { return a + b; }
