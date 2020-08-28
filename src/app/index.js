434ea12d2c5c45ae2d5ebf8f037e5338532a4dcd
Slava <19uvs99@gmail.com>
2020-08-28 12:53:35 +0300

    Task 2 Done

--- src/app/index.js
+++ src/app/index.js
@@ -1,8 +1,9 @@
 import './assets/styles/main.scss';
+/*eslint-disable */
 
 // Task 1
-const populationList = document.querySelectorAll('[class = population]');
-const populationArr = Array.prototype.slice.call(populationList);
+const populationList = document.querySelectorAll('.population');
+const populationArr = [...populationList];
 
 const totalPop = populationArr.reduce((sum, curEl) => {
   const el = curEl.innerHTML.replace(/,/g, '');
@@ -12,7 +13,35 @@ const totalPop = populationArr.reduce((sum, curEl) => {
 const averagePop = totalPop / populationList.length;
 function prettify(num) {
   const sNum = num.toString();
-  return sNum.replace(/(\d{1,3}(?=(?:\d\d\d)+(?!\d)))/g, '$1,');
+  return sNum.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
 document.querySelector('[class = total-population]').innerHTML = prettify(totalPop);
 document.querySelector('[class = average-population]').innerHTML = prettify(averagePop);
+
+// Task 2
+
+const liArr = document.querySelectorAll('ul li');
+const newArr = [...liArr];
+const newObj = newArr.map((el) => {
+  const salaryNum = Number(el.dataset.salary.replace(/[$,]/g, ''));
+  return {
+    position: el.dataset.position,
+    salary: salaryNum,
+    age: el.dataset.age,
+    worker: el.innerText,
+  };
+});
+
+const honorWorker = newObj.reduce(((previousWorker, currentWorker) => {
+  return currentWorker.salary > previousWorker.salary ? currentWorker : previousWorker
+}), {salary: null});
+
+function numberToStringAndPrettify (num){
+  return String(num).replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+}
+
+document.querySelector('#name').innerText = honorWorker.worker;
+document.querySelector('#salary').innerText ='$' + numberToStringAndPrettify(honorWorker.salary);
+document.querySelector('#age').innerText = honorWorker.age + ' года';
+document.querySelector('#position').innerText = honorWorker.position;
+
