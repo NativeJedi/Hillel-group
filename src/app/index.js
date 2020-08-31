a06ea70c56e3d3b01c1933f866052fc0a98a89d7
Dk1775 <dmytro.karachan@gmail.com>
2020-08-31 22:05:33 +0300

    remove for-in, changed forEach for map

--- src/app/index.js
+++ src/app/index.js
@@ -1,16 +1,14 @@
 /* eslint-disable guard-for-in */
 import './assets/styles/main.scss';
 
-const listItems = document.querySelectorAll('span.population');
-const populArr = [];
+const listItems = Array.from(document.querySelectorAll('span.population'));
 
-Array.from(listItems).forEach((item) => {
+const populArr = listItems.map((item) => {
   const itemNum = Number(item.textContent.replace(/,/g, ''));
-  populArr.push(itemNum);
+  return itemNum;
 });
-
 const total = populArr.reduce((acc, currVal) => acc + currVal, 0);
-const average = populArr.reduce((acc, currVal) => Math.round((acc + currVal) / populArr.length), 0);
+const average = total / populArr.length;
 document.querySelector('p.total > span').innerText = total;
 document.querySelector('p.average > span').innerText = average;
 
@@ -43,9 +41,6 @@ employeesToArr(employeesList);
 
 const topSalaryEmployee = arr.reduce((prev, curr) => (prev.salary > curr.salary ? prev : curr));
 
-// eslint-disable-next-line no-restricted-syntax
-for (const key in honorDesk) {
-  if (Object.prototype.hasOwnProperty.call(honorDesk, key)) {
-    honorDesk[key].inner = topSalaryEmployee[key];
-  }
-}
+Object.keys(honorDesk).forEach((key) => {
+  honorDesk[key].innerText = topSalaryEmployee[key];
+});
