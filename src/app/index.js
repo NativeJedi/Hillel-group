e66c679772b5dfc71739a83a665ff43cdcd2e57a
Slava <19uvs99@gmail.com>
2020-09-01 01:19:40 +0300

    todo list alfa 0.1 =)

--- src/app/index.js
+++ src/app/index.js
@@ -1,47 +1,29 @@
-import './assets/styles/main.scss';
-/*eslint-disable */
-
-// Task 1
-const populationList = document.querySelectorAll('.population');
-const populationArr = [...populationList];
-
-const totalPop = populationArr.reduce((sum, curEl) => {
-  const el = curEl.innerHTML.replace(/,/g, '');
-  return sum + Number(el);
-}, 0);
-
-const averagePop = totalPop / populationList.length;
-function prettify(num) {
-  const sNum = num.toString();
-  return sNum.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+const addButton = document.getElementById('add-button');
+const input = document.getElementById('input');
+const ol = document.getElementById('List');
+const delButton = document.getElementById('del-button');
+// const output = document.getElementById('output');
+function adLiOnList() {
+  const liLast = document.createElement('li');
+  liLast.innerHTML = input.value;
+  input.value = '';
+  ol.append(liLast);
 }
-document.querySelector('.total-population').innerHTML = prettify(totalPop);
-document.querySelector('.average-population').innerHTML = prettify(averagePop);
 
-// Task 2
-
-const liArr = document.querySelectorAll('ul li');
-const newArr = [...liArr];
-const newObj = newArr.map((el) => {
-  const salaryNum = Number(el.dataset.salary.replace(/[$,]/g, ''));
-  return {
-    position: el.dataset.position,
-    salary: salaryNum,
-    age: el.dataset.age,
-    worker: el.innerText,
-  };
+addButton.addEventListener('click', () => {
+  if (input.value !== '') {
+    adLiOnList();
+  }
 });
 
-const honorWorker = newObj.reduce(((previousWorker, currentWorker) => {
-  return currentWorker.salary > previousWorker.salary ? currentWorker : previousWorker
-}), {salary: null});
-
-function numberToStringAndPrettify (num){
-  return String(num).replace(/\B(?=(\d{3})+(?!\d))/g, ',')
-}
-
-document.querySelector('#name').innerText = honorWorker.worker;
-document.querySelector('#salary').innerText ='$' + numberToStringAndPrettify(honorWorker.salary);
-document.querySelector('#age').innerText = honorWorker.age + ' года';
-document.querySelector('#position').innerText = honorWorker.position;
-
+input.addEventListener('keydown', (e) => {
+  if (e.code === 'Enter') {
+    e.preventDefault();
+    if (input.value !== '') {
+      e.preventDefault();
+      adLiOnList();
+    }
+  }
+});
+delButton.addEventListener('click', () => {
+});
