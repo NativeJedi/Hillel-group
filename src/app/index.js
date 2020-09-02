b8de3245546c527bdcf276fd9eed0e7ae8cb00c1
Slava <19uvs99@gmail.com>
2020-09-02 15:29:32 +0300

    HW DONE

--- src/app/index.js
+++ src/app/index.js
@@ -1,11 +1,16 @@
+import('./assets/styles/main.scss');
+
 const addButton = document.getElementById('add-button');
 const input = document.getElementById('input');
-const ol = document.getElementById('List');
+const ol = document.getElementById('list');
 const delButton = document.getElementById('del-button');
-// const output = document.getElementById('output');
+const list = document.querySelector('#list');
+const delSelButton = document.getElementById('del-selected-button');
+
 function adLiOnList() {
   const liLast = document.createElement('li');
   liLast.innerHTML = input.value;
+  liLast.classList.add('list-item');
   input.value = '';
   ol.append(liLast);
 }
@@ -25,5 +30,25 @@ input.addEventListener('keydown', (e) => {
     }
   }
 });
+
+list.addEventListener('click', (e) => {
+  const el = e.target.closest('.list-item');
+  if (!el) {
+    return;
+  }
+  el.classList.add('selected');
+  delSelButton.addEventListener('click', () => {
+    const selectedEl = document.querySelectorAll('.selected');
+    selectedEl.forEach((elem) => {
+      elem.parentNode.removeChild(elem);
+    });
+  });
+});
+
 delButton.addEventListener('click', () => {
+  const listElements = document.querySelectorAll('.list-item');
+  console.log(listElements);
+  listElements.forEach((el) => {
+    el.parentNode.removeChild(el);
+  });
 });
