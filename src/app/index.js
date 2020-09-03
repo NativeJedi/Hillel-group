cd9e36a89f1013799bf0f8e6fee09545f091ff26
javastriptizer <65609455+javastriptizer@users.noreply.github.com>
2020-09-03 16:35:31 +0300

    Task 2

--- src/app/index.js
+++ src/app/index.js
@@ -20,3 +20,41 @@ const average = fixNumber(aver);
 
 document.querySelector('[class = total-population]').innerHTML = total;
 document.querySelector('[class = average-population]').innerHTML = average;
+
+// Task 2
+
+const salaryELements = document.querySelectorAll('li[data-salary]');
+
+const honorDesk = {
+  name: '',
+  salary: 0,
+  age: 0,
+  position: '',
+};
+
+const topEmployee = [...salaryELements].reduce((desk, employeeEl) => {
+  const {
+    salary,
+    age,
+    position,
+  } = employeeEl.dataset;
+
+  const name = employeeEl.innerText;
+  const salaryAmount = +salary.replace(/[, $]/g, '');
+
+  if (desk.salary > salaryAmount) {
+    return desk;
+  }
+
+  return {
+    name,
+    salary: salaryAmount,
+    age,
+    position,
+  };
+}, honorDesk);
+
+document.querySelector('[id = name]').innerHTML = `- ${topEmployee.name}`;
+document.querySelector('[id = salary]').innerHTML = `- ${topEmployee.salary}`;
+document.querySelector('[id = age]').innerHTML = `- ${topEmployee.age}`;
+document.querySelector('[id = position]').innerHTML = `- ${topEmployee.position}`;
